632e270d0e714dc31e36566ae9c16fc9ceb687b6
elpachongco <earlsiachongco@gmail.com>
2024-10-02 21:40:11 +0800

    Change theme toggle to show buttons for dark and light mode

--- static/js/script.js
+++ static/js/script.js
@@ -1,216 +1,251 @@
 /* Themes are copied from the MonkeyType repo
  * https://github.com/monkeytypegame/monkeytype/tree/master/frontend/static/themes
+ * Filenames segrated by theme using the script css/themes/segregate.py
  */
-let themes = [
-  "8008.css",
-  "80s_after_dark.css",
-  "9009.css",
-  "aether.css",
-  "alduin.css",
-  "alpine.css",
-  "anti_hero.css",
-  "arch.css",
-  "aurora.css",
-  "beach.css",
-  "bento.css",
-  "bingsu.css",
-  "bliss.css",
-  "blueberry_dark.css",
-  "blueberry_light.css",
-  "blue_dolphin.css",
-  "botanical.css",
-  "bouquet.css",
-  "breeze.css",
-  "bushido.css",
-  "cafe.css",
-  "camping.css",
-  "carbon.css",
-  "catppuccin.css",
-  "chaos_theory.css",
-  "cheesecake.css",
-  "cherry_blossom.css",
-  "comfy.css",
-  "copper.css",
-  "creamsicle.css",
-  "cyberspace.css",
-  "cy_red.css",
-  "dark.css",
-  "dark_magic_girl.css",
-  "dark_note.css",
-  "darling.css",
-  "deku.css",
-  "desert_oasis.css",
-  "dev.css",
-  "diner.css",
-  "dino.css",
-  "discord.css",
-  "dmg.css",
-  "dollar.css",
-  "dots.css",
-  "dracula.css",
-  "drowning.css",
-  "dualshot.css",
-  "earthsong.css",
-  "everblush.css",
-  "evil_eye.css",
-  "ez_mode.css",
-  "fire.css",
-  "fledgling.css",
-  "fleuriste.css",
-  "floret.css",
-  "froyo.css",
-  "frozen_llama.css",
-  "fruit_chew.css",
-  "fundamentals.css",
-  "future_funk.css",
-  "godspeed.css",
-  "graen.css",
-  "grand_prix.css",
-  "grape.css",
-  "gruvbox_dark.css",
-  "gruvbox_light.css",
-  "hammerhead.css",
-  "hanok.css",
-  "hedge.css",
-  "honey.css",
-  "horizon.css",
-  "husqy.css",
-  "iceberg_dark.css",
-  "iceberg_light.css",
-  "incognito.css",
-  "ishtar.css",
-  "iv_clover.css",
-  "iv_spade.css",
-  "joker.css",
-  "laser.css",
-  "lavender.css",
-  "leather.css",
-  "lilac_mist.css",
-  "lil_dragon.css",
-  "lime.css",
-  "luna.css",
-  "macroblank.css",
-  "magic_girl.css",
-  "mashu.css",
-  "matcha_moccha.css",
-  "material.css",
-  "matrix.css",
-  "menthol.css",
-  "metaverse.css",
-  "metropolis.css",
-  "mexican.css",
-  "miami.css",
-  "miami_nights.css",
-  "midnight.css",
-  "milkshake.css",
-  "mint.css",
-  "mizu.css",
-  "modern_dolch.css",
-  "modern_dolch_light.css",
-  "modern_ink.css",
-  "monokai.css",
-  "moonlight.css",
-  "mountain.css",
-  "mr_sleeves.css",
-  "ms_cupcakes.css",
-  "muted.css",
-  "nautilus.css",
-  "nebula.css",
-  "night_runner.css",
-  "nord.css",
-  "nord_light.css",
-  "norse.css",
-  "oblivion.css",
-  "olive.css",
-  "olivia.css",
-  "onedark.css",
-  "our_theme.css",
-  "paper.css",
-  "passion_fruit.css",
-  "pastel.css",
-  "peach_blossom.css",
-  "peaches.css",
-  "pink_lemonade.css",
-  "pulse.css",
-  "purpleish.css",
-  "rainbow_trail.css",
-  "red_dragon.css",
-  "red_samurai.css",
-  "repose_dark.css",
-  "repose_light.css",
-  "retrocast.css",
-  "retro.css",
-  "rgb.css",
-  "rose_pine.css",
-  "rose_pine_dawn.css",
-  "rose_pine_moon.css",
-  "rudy.css",
-  "ryujinscales.css",
-  "serika.css",
-  "serika_dark.css",
-  "sewing_tin.css",
-  "sewing_tin_light.css",
-  "shadow.css",
-  "shoko.css",
-  "slambook.css",
-  "snes.css",
-  "soaring_skies.css",
-  "solarized_dark.css",
-  "solarized_light.css",
-  "sonokai.css",
-  "stealth.css",
-  "strawberry.css",
-  "striker.css",
-  "superuser.css",
-  "sweden.css",
-  "tangerine.css",
-  "taro.css",
-  "terminal.css",
-  "terra.css",
-  "terrazzo.css",
-  "terror_below.css",
-  "tiramisu.css",
-  "trackday.css",
-  "trance.css",
-  "tron_orange.css",
-  "vaporwave.css",
-  "viridescent.css",
-  "voc.css",
-  "vscode.css",
-  "watermelon.css",
-  "wavez.css",
-  "witch_girl.css",
-];
-themeToggler = document.getElementById("theme-toggle");
+
+let themes = {
+  dark: [
+    "mountain.css",
+    "blue_dolphin.css",
+    "joker.css",
+    "anti_hero.css",
+    "dots.css",
+    "trance.css",
+    "earthsong.css",
+    "peach_blossom.css",
+    "material.css",
+    "superuser.css",
+    "metropolis.css",
+    "aether.css",
+    "olivia.css",
+    "husqy.css",
+    "miami_nights.css",
+    "wavez.css",
+    "future_funk.css",
+    "stealth.css",
+    "bouquet.css",
+    "sonokai.css",
+    "fire.css",
+    "voc.css",
+    "rgb.css",
+    "catppuccin.css",
+    "rose_pine.css",
+    "red_dragon.css",
+    "arch.css",
+    "drowning.css",
+    "vscode.css",
+    "incognito.css",
+    "monokai.css",
+    "pulse.css",
+    "viridescent.css",
+    "tron_orange.css",
+    "chaos_theory.css",
+    "ryujinscales.css",
+    "norse.css",
+    "bliss.css",
+    "laser.css",
+    "dracula.css",
+    "80s_after_dark.css",
+    "alduin.css",
+    "floret.css",
+    "carbon.css",
+    "aurora.css",
+    "bushido.css",
+    "grape.css",
+    "nord.css",
+    "solarized_dark.css",
+    "moonlight.css",
+    "matrix.css",
+    "night_runner.css",
+    "horizon.css",
+    "cyberspace.css",
+    "mint.css",
+    "modern_dolch.css",
+    "dark_note.css",
+    "hammerhead.css",
+    "dark_magic_girl.css",
+    "luna.css",
+    "purpleish.css",
+    "blueberry_dark.css",
+    "watermelon.css",
+    "nebula.css",
+    "iv_spade.css",
+    "nautilus.css",
+    "gruvbox_dark.css",
+    "mashu.css",
+    "shadow.css",
+    "rudy.css",
+    "ishtar.css",
+    "terror_below.css",
+    "terminal.css",
+    "terra.css",
+    "oblivion.css",
+    "metaverse.css",
+    "dev.css",
+    "dark.css",
+    "iceberg_dark.css",
+    "midnight.css",
+    "everblush.css",
+  ],
+  light: [
+    "sweden.css",
+    "cherry_blossom.css",
+    "dualshot.css",
+    "graen.css",
+    "camping.css",
+    "strawberry.css",
+    "ez_mode.css",
+    "alpine.css",
+    "slambook.css",
+    "cafe.css",
+    "mizu.css",
+    "olive.css",
+    "our_theme.css",
+    "peaches.css",
+    "miami.css",
+    "evil_eye.css",
+    "taro.css",
+    "iceberg_light.css",
+    "honey.css",
+    "deku.css",
+    "repose_dark.css",
+    "lime.css",
+    "sewing_tin_light.css",
+    "breeze.css",
+    "soaring_skies.css",
+    "modern_ink.css",
+    "muted.css",
+    "9009.css",
+    "nord_light.css",
+    "dmg.css",
+    "darling.css",
+    "solarized_light.css",
+    "diner.css",
+    "pink_lemonade.css",
+    "fleuriste.css",
+    "rose_pine_dawn.css",
+    "menthol.css",
+    "gruvbox_light.css",
+    "discord.css",
+    "frozen_llama.css",
+    "onedark.css",
+    "modern_dolch_light.css",
+    "hanok.css",
+    "retrocast.css",
+    "cheesecake.css",
+    "fledgling.css",
+    "milkshake.css",
+    "tangerine.css",
+    "dollar.css",
+    "paper.css",
+    "serika_dark.css",
+    "dino.css",
+    "mexican.css",
+    "red_samurai.css",
+    "comfy.css",
+    "lilac_mist.css",
+    "vaporwave.css",
+    "bento.css",
+    "retro.css",
+    "magic_girl.css",
+    "rainbow_trail.css",
+    "ms_cupcakes.css",
+    "lavender.css",
+    "beach.css",
+    "passion_fruit.css",
+    "mr_sleeves.css",
+    "repose_light.css",
+    "pastel.css",
+    "froyo.css",
+    "leather.css",
+    "grand_prix.css",
+    "striker.css",
+    "shoko.css",
+    "trackday.css",
+    "desert_oasis.css",
+    "cy_red.css",
+    "botanical.css",
+    "creamsicle.css",
+    "tiramisu.css",
+    "matcha_moccha.css",
+    "rose_pine_moon.css",
+    "iv_clover.css",
+    "godspeed.css",
+    "lil_dragon.css",
+    "bingsu.css",
+    "terrazzo.css",
+    "blueberry_light.css",
+    "copper.css",
+    "8008.css",
+    "hedge.css",
+    "macroblank.css",
+    "witch_girl.css",
+    "serika.css",
+    "fundamentals.css",
+    "snes.css",
+    "fruit_chew.css",
+    "sewing_tin.css",
+  ],
+};
+
+
+let darkThemeToggler = document.getElementById("dark-theme-btn");
+let lightThemeToggler = document.getElementById("light-theme-btn");
+let themeBtnSeparator = document.getElementsByClassName("theme-btn-separator")[0]
+let themeNameDisplay = document.getElementById("theme-name-display");
 
 let themeStyleElement = document.getElementById("theme-link");
 
-let savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-  themeStyleElement.href = savedTheme;
+let timeout = null;
+let currentThemeIdx = {
+  "dark": Number(localStorage.getItem("theme-dark-idx") || 1), 
+  "light": Number(localStorage.getItem("theme-light-idx") || 1)
+}
+let currentMode = localStorage.getItem("theme-mode") || "dark"
+
+applyTheme(currentMode, currentThemeIdx[currentMode], false);
+
+darkThemeToggler.addEventListener("click", function(_) {toggleTheme("dark", currentThemeIdx["dark"])})
+lightThemeToggler.addEventListener("click", function(_) {toggleTheme("light", currentThemeIdx["light"])})
+
+function toggleTheme(mode, modeThemeIdx) {
+  if (modeThemeIdx == currentThemeIdx[mode] && mode == currentMode) {
+    let newThemeIdx = modeThemeIdx + 1 < themes[mode].length ? modeThemeIdx + 1 : 0
+    currentThemeIdx[mode] = newThemeIdx
+  }
+  applyTheme(mode, currentThemeIdx[mode] || 0)
+  currentMode = mode
 }
 
-let timeout = null;
-themeToggler.addEventListener("click", function(_) {
-  // Do this here, not in the stylesheet to prevent transition on page load.
-  document.body.style.transition = "background-color .4s linear"
-
-  // Returns theme.css from localhost:1313/css/themes/theme.css 
-  let prevThemeName = themeStyleElement.href.split("/").pop();
-
-  let themeIdx = themes.indexOf(prevThemeName);
-
-  let newThemeName = themeIdx + 1 < themes.length ? themes[themeIdx + 1] : themes[0]
-  let cssPath = `/css/themes/${newThemeName}`;
+function applyTheme(mode, idx, showTheme=true) {
+  if (idx >= themes[mode].length || idx < 0) {
+    throw RangeError
+  }
+  let cssPath = `/css/themes/${themes[mode][idx]}`;
   themeStyleElement.href = cssPath;
 
-  localStorage.setItem("theme", cssPath);
+  if (!showTheme) return;
 
-  themeToggler.innerText = newThemeName;
+  localStorage.setItem("theme-mode", mode);
+  localStorage.setItem("theme-dark-idx", currentThemeIdx["dark"]);
+  localStorage.setItem("theme-light-idx", currentThemeIdx["light"]);
+
+  darkThemeToggler.style.display = "none";
+  lightThemeToggler.style.display = "none";
+  themeBtnSeparator.style.display = "none";
 
   if (timeout) {
     clearInterval(timeout)
   }
 
-  timeout = setTimeout(() => { themeToggler.innerText = "Theme"; }, 2000)
-});
+  themeNameDisplay.innerText = themes[mode][idx].replace(".css", "").replaceAll("_", " ");
+  ;
+  themeNameDisplay.style.display = "inline";
+
+  timeout = setTimeout(() => { 
+    themeNameDisplay.style.display = "none";
+    darkThemeToggler.style.display = "inline";
+    lightThemeToggler.style.display = "inline";
+    themeBtnSeparator.style.display = "inline";
+  }, 500)
+}
